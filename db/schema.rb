16e1b2b3d1f8a8de2d181c0e426324d033e2f89a
Luis Contreras Guerra <luisguerra495@gmail.com>
2019-04-22 15:50:30 -0500

    Scaffold for Topic

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_22_032314) do
+ActiveRecord::Schema.define(version: 2019_04_22_204927) do
 
   create_table "courses", force: :cascade do |t|
     t.string "name"
     t.string "description"
     t.string "category"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "topics", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
