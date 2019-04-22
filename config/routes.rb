34beffdce1339991c5cd097ee8d3812a9240b33c
Luis Contreras Guerra <luisguerra495@gmail.com>
2019-04-22 15:35:27 -0500

    Add route for dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   resources :courses
 
+  get "/dashboard" => "dashboard#index"
+
   root "courses#courses_index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
