5f6c666d4fa37ef1e874dd7625546a6c621da7f3
Luis Contreras Guerra <luisguerra495@gmail.com>
2019-04-30 14:11:33 -0500

    Froala and other stuffs

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,9 @@ Rails.application.routes.draw do
 
   get "/course/play/:course_id" => "course_in_progress#index"
 
+  post "/upload_image" => "upload#upload_image", :as => :upload_image
+  get "/download_file/:name" => "upload#access_file", :as => :upload_access_file, :name => /.*/
+
   root "courses#courses_index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
