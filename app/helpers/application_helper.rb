cb91646cf0089f25d5781ef7227cc39cc64dfcab
Luis Contreras Guerra <luisguerra495@gmail.com>
2019-05-06 18:48:43 -0500

    Fix helper bug and remove links to users that are not admin

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,7 @@
 module ApplicationHelper
     def is_admin(current_user)
-        current_user.role == "admin"
+        if current_user
+            current_user.role == "admin"
+        end
     end
 end
