1f361e7d7fe2df0cfcfe2bc31fe6e24fe7b50631
Luis Contreras Guerra <luisguerra495@gmail.com>
2019-05-05 20:34:10 -0500

    New modifications to user profile

--- app/controllers/user_profile_controller.rb
+++ app/controllers/user_profile_controller.rb
@@ -2,7 +2,7 @@ class UserProfileController < ApplicationController
     before_action :set_params
 
     def index
-        @courses = CourseUser.where(user_id: params[:id])
+        @courses_user = CourseUser.joins(:course).where(user_id: params[:id])
     end
 
     private
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+    def is_admin(current_user)
+        current_user.role == "admin"
+    end
 end
