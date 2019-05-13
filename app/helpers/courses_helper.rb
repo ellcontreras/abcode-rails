9293069492404b9521b6c2b907ef56a763d0b3c1
Luis Contreras Guerra <luisguerra495@gmail.com>
2019-05-13 17:28:01 -0500

    Fix bug that does not count the percentage

--- app/helpers/courses_helper.rb
+++ app/helpers/courses_helper.rb
@@ -4,39 +4,33 @@ module CoursesHelper
 
         topics = Topic.joins(:lessons, :course).where(course_id: course_id)
 
-        already_actual_lesson = false
-        already_actual_topic = false
+        already_actual_lesson = true
+        already_actual_topic = true
         number_actual_lesson = 0
         number_actual_topic = 0
         percent = 0
 
         topics.each do |topic|
-            if !already_actual_lesson
-                topic.lessons.each do |lesson|
+            if already_actual_lesson
                 number_actual_lesson += 1
-                    if lesson.id == courses_user.course_progress[0].lesson_id
-                        @already_actual_lesson = true
+                topic.lessons.each do |lesson|
+                    if lesson.id == courses_user.course_progress.first.lesson_id
+                        @already_actual_lesson = false
                     end
                 end
             end
-        end
 
-        topics.each do |topic|
-            if !already_actual_topic
+            if already_actual_topic
                 number_actual_topic += 1 
-                if topic.id == courses_user.course_progress[0].topic_id
-                    @already_actual_topic = true
+                if topic.id == courses_user.course_progress.first.topic_id
+                    @already_actual_topic = false
                 end
             end
         end
 
         topics.each do |topic|
             if topic.course_id == course_id
-                if (topic.lessons.length == 1 and topics.length == 1)
-                    percent = ((number_actual_lesson - 1) + (number_actual_topic - 1)) * 100 / (topic.lessons.length + topics.length)
-                else
-                    percent = ((number_actual_lesson + number_actual_topic)) * 100 / (topic.lessons.length + topics.length)
-                end
+                percent = ((number_actual_lesson + number_actual_topic) / (topic.lessons.length + topics.length)) * 100
             end
         end
 
